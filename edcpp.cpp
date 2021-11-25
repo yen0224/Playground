76d3a08bdf3c512a02d77d93290ec12d35a9d5ad
yen <yen0224@pm.me>
2021-11-25 09:30:41 +0800

    stable version **bug fixed

--- edcpp.cpp
+++ edcpp.cpp
@@ -6,12 +6,24 @@ using namespace std;
 class route{
     public:
         double snode, enode,limit;
+        int intsnode,intenode;
         double rl,rw;
 
         void setValue(double,double,double,double,double);
         bool exam(int);
         void print();
+        void transfer(int);
 }Sample;
+void route::transfer(int result){
+    if(result){
+    intsnode=int(snode);
+    intenode=int(enode);
+    }
+    else{
+        intsnode=0;
+        intenode=0;
+    }
+}
 void route::setValue(double s,double e,double l,double w,double lm){
     snode=s;
     enode=e;
@@ -52,46 +64,42 @@ void route::print(){
 
 int main(int argc, char const *argv[])
 {
+    int n;
+    
     vector<int> starts,ends,ls,ws,limits;
     //open files
     ifstream inputfile;
     inputfile.open("test3.txt");
-    if(!inputfile.is_open()){
-        cout<<"Failed to open files, program stopped";
-        return 0;//end program
-    }
-    else{
-        int n;
-        //nodemap:[0]road length [1]road width [2]limit
+    //nodemap:[0]road length [1]road width [2]limit
         inputfile>>n;
-        float nodemap[n][n][3];
+        cout<<"n="<<n<<endl;
+        double nodemap[n][n][3];
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                nodemap[i][j][0]=999999;
-                nodemap[i][j][1]=999999;
-                nodemap[i][j][2]=999999;
+                nodemap[i][j][0]=-1;
+                nodemap[i][j][1]=-1;
+                nodemap[i][j][2]=-1;
             }
             
         }
-        double s,e,l,w,lm,sw;
+        
+        double s,e,l,w,lm;
         while (inputfile>>s>>e>>l>>w>>lm){
+            int is,ie;
             //inputfile>>s>>e>>l>>w>>lm;
-            if(s>e){
-                sw=s;
-                s=e;
-                e=sw;
-            }
             Sample.setValue(s,e,l,w,lm);
-            int result = Sample.exam(n);
+            bool result = Sample.exam(n);
+            Sample.transfer(result);
             if (result){
                 //TODO 缺驗證內容，以路寬為基準，限制為輔
-                int ints=int(s);
-                int inte=int(e);
-                nodemap[ints][inte][0]=l;
-                nodemap[ints][inte][1]=w;
-                nodemap[ints][inte][2]=lm;
+                int is=s;
+                int ie=e;
+                cout<<"write position:"<<is<<","<<ie<<"info"<<l<<w<<lm<<endl;
+                nodemap[is][ie][0]=l;
+                //nodemap[is][ie][1]=w;
+                //nodemap[is][ie][2]=lm;
             }
             cout<<s<<" "<<e<<" "<<l<<" "<<w<<" "<<lm<<" "<<(result?"Valid":"Invalid")<<endl;
             starts.push_back(s);
@@ -101,7 +109,22 @@ int main(int argc, char const *argv[])
             limits.push_back(lm);
         }
         inputfile.close();
-    }
+        
+    
+    for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout<<i<<","<<j<<" ";
+                
+                cout<<"\t"<<nodemap[i][j][0]<<" ";
+                /*cout<<nodemap[i][j][1]<<" ";
+                cout<<nodemap[i][j][2]<<" ";*/
+           }
+            cout<<endl;  
+        }
+    
+    
     cout<<"Please input start node, end node, and approach:";
     int rq_s,rq_e,rq_a;
     cin>>rq_s>>rq_e>>rq_a;
